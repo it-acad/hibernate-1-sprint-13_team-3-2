a127dc22eb1ddaf5feea3fd780700918eb1052e5
romonys <roma.miskovec@gmail.com>
2021-01-12 00:32:46 +0200

    Class State

--- src/main/java/com/softserve/itacademy/model/State.java
+++ src/main/java/com/softserve/itacademy/model/State.java
@@ -1,5 +1,63 @@
 package com.softserve.itacademy.model;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+@Entity
+@Table(name = "states")
 public class State {
- //TODO
+
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "state_sequence"),
+                    @Parameter(name = "initial_value", value = "1"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private long id;
+
+    @NotBlank(message = "The stateName cannot be empty")
+    @Column(nullable = false, unique = true)
+    @Pattern(regexp = "[A-Za-z\\d\\s-_]{1,20}")
+    private String name;
+
+    @OneToMany(mappedBy = "state")
+    private List<Task> tasks;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "Role {" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                "} ";
+    }
 }
