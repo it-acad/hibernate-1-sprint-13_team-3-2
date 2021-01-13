04767c2977a22ad56ba740cb609c1332f9b787e0
Starov Nikita <starovnikita15@gmail.com>
2021-01-13 19:33:47 +0200

    StateTest

---
+++ src/main/java/com/softserve/itacademy/model/StateTest.java
@@ -0,0 +1,27 @@
+package com.softserve.itacademy.model;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class StateTest {
+    @Test
+    void constraintViolationOnEmptyRoleName() {
+        State emptyState = new State();
+        emptyState.setName("");
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<State>> violations = validator.validate(emptyState);
+        assertEquals(1, violations.size());
+    }
+}
