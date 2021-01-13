4e29685aaca52f9bfec64361105d17b70739aff9
Starov Nikita <starovnikita15@gmail.com>
2021-01-13 19:53:44 +0200

    ToDoTest

---
+++ src/main/java/com/softserve/itacademy/model/ToDoTest.java
@@ -0,0 +1,88 @@
+package com.softserve.itacademy.model;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.params.provider.Arguments;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Stream;
+
+@SpringBootTest
+public class ToDoTest {
+
+    private static Role traineeRole;
+    private static User user;
+    private static ToDo validToDo;
+
+    @BeforeAll
+    static void init(){
+        traineeRole = new Role();
+        traineeRole.setName("TRAINEE");
+        user = new User();
+        user.setEmail("valid@cv.edu.ua");
+        user.setFirstName("Valid-Name");
+        user.setLastName("Valid-Name");
+        user.setPassword("qwQW12!@");
+        user.setRole(traineeRole);
+        validToDo.setOwner(user);
+        validToDo.setTitle("Valid-Title");
+        validToDo.setCreatedAt(LocalDateTime.now());
+    }
+    @Test
+    void toDoWithValidTitle() {
+        ToDo toDo = new ToDo();
+        toDo.setTitle("Valid-Title");
+        toDo.setOwner(user);
+        toDo.setCreatedAt(LocalDateTime.now());
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<ToDo>> violations = validator.validate(toDo);
+
+        assertEquals(0, violations.size());
+    }
+
+    @Test
+    void createValidToDo(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<ToDo>> violations = validator.validate(validToDo);
+
+        assertEquals(0, violations.size());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidTitleToDo")
+    void constraintViolationInvalidTitle(String input, String errorValue) {
+        ToDo toDo = new ToDo();
+        toDo.setTitle(input);
+        toDo.setOwner(user);
+        toDo.setCreatedAt(LocalDateTime.now());
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<ToDo>> violations = validator.validate(toDo);
+        assertEquals(1, violations.size());
+        assertEquals(errorValue, violations.iterator().next().getInvalidValue());
+    }
+
+    private static Stream<Arguments> provideInvalidTitleToDo(){
+        return Stream.of(
+                Arguments.of("invalid", "invalid"),
+                Arguments.of("Invalid-", "Invalid-"),
+                Arguments.of("Invalid-invalid", "Invalid-invalid")
+        );
+    }
+}
